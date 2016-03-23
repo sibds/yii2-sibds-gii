170c1180d968f3a7899b7c227cb50e64e9638b6a
Vadim Mazur <mazurva@gmail.com>
2016-03-23 16:48:44 +0600

    Added files via upload

---
+++ gii/module/default/message.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * This is the template for generating a message file within a module.
+ */
+/* @var $this yii\web\View */
+/* @var $generator yii\gii\generators\module\Generator */
+echo "<?php\n";
+?>
+return [];
