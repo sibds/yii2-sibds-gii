a1be872c74db1396a9ae689c35224c4e7fc85c33
Vadim Mazur <mazurva@gmail.com>
2016-04-17 18:29:05 +0600

    Update _form.php

--- gii/crud/default/views/_form.php
+++ gii/crud/default/views/_form.php
@@ -36,7 +36,7 @@ use yii\widgets\ActiveForm;
 
     <?php foreach ($generator->getColumnNames() as $attribute) {
         if(in_array($attribute, ['created_at', 'created_by', 'updated_at', 'updated_by',
-        'tree', 'lft', 'rgt', 'depth']))
+        'tree', 'lft', 'rgt', 'depth', 'order']))
             continue;
 
         if (in_array($attribute, $safeAttributes)) {
