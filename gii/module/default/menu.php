7eeafa2624a18b938046f617bd4e1a68da3b3f7b
Vadim Mazur <mazurva@gmail.com>
2016-04-17 18:08:50 +0600

    Create menu.php

---
+++ gii/module/default/menu.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vadim
+ * Date: 17.04.16
+ * Time: 18:03
+ */
+/* @var $this yii\web\View */
+/* @var $generator yii\gii\generators\module\Generator */
+$className = $generator->moduleClass;
+$pos = strrpos($className, '\\');
+$ns = ltrim(substr($className, 0, $pos), '\\');
+$className = substr($className, $pos + 1);
+$moduleId = $generator->moduleID;
+echo "<?php\n";
+?>
+return [
+['label' => '<?=$className?>', 'url' => ['/<?=$moduleId?>/<?=$moduleId?>/list'], 'icon' => 'fa fa-circle-o'],
+];
